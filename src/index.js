b620aa92413ba0d3073c87df7b142b9254a62be0
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-03-27 23:25:50 -0700

    add control placeholders

--- src/index.js
+++ src/index.js
@@ -17,15 +17,37 @@ function drawClaw(ctx){
 }
 
 function drawBarrier(ctx){
-  ctx.fillStyle = "#000000"
-  ctx.fillRect(125, 250, 5, 300);
-  ctx.fillRect(125, 450, 350, 5)
+  ctx.fillStyle = "black"
+  ctx.fillRect(125, 350, 350, 100)
+  ctx.fillStyle = "#757373"
+  ctx.fillRect(125, 190, 5, 300);
+  ctx.fillRect(125, 450, 350, 100);
+  ctx.fillRect(0, 400, 130, 150);
+  ctx.fillStyle = "#474747"
+  ctx.fillRect(20, 450, 90, 90)
+}
+
+function drawControls(ctx){
+  ctx.fillStyle = "white"
+  ctx.beginPath();
+  ctx.moveTo(190, 400);
+  ctx.lineTo(230, 380);
+  ctx.lineTo(230, 420);
+  ctx.fill();
+  ctx.fillStyle = "green"
+  ctx.fillRect(250, 380, 70, 40)
+  ctx.fillStyle = "white"
+  ctx.beginPath();
+  ctx.moveTo(380, 400);
+  ctx.lineTo(340, 380);
+  ctx.lineTo(340, 420);
+  ctx.fill();
 }
 
 function drawAll(ctx){
   drawClaw(ctx);
   drawBarrier(ctx);
-
+  drawControls(ctx);
 }
 
 function gameLoop(timestamp) {
