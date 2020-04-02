900ee70b95e45dfdc5269400c0794ed87d4ee71b
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-02 00:17:29 -0700

    fix deploy script & add button text

--- src/control.js
+++ src/control.js
@@ -1,13 +1,20 @@
 export default class Control {
     draw(ctx){
+        // left arrow
         ctx.fillStyle = "white"
         ctx.beginPath();
         ctx.moveTo(190, 420);
         ctx.lineTo(230, 400);
         ctx.lineTo(230, 440);
         ctx.fill();
-        ctx.fillStyle = "green"
+        // drop button
+        ctx.fillStyle = "lightgreen"
         ctx.fillRect(250, 400, 70, 40)
+        // drop button text
+        ctx.fillStyle = "black";
+        ctx.font = "bold 18px arial";
+        ctx.fillText("DROP", 260, 426);
+        // right arrow
         ctx.fillStyle = "white"
         ctx.beginPath();
         ctx.moveTo(380, 420);
